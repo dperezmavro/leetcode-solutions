3d49ca566824a1deac1ea17810b2f6754d9c29d7
dperezmavro <dio.pm@live.com>
2026-04-25 14:27:57 +0700

    added rust tests

---
+++ easy/1732-find-the-highest-altitude/rust/tests/solution_test.rs
@@ -0,0 +1,24 @@
+use find_the_highest_altitude::solution;
+
+struct TestCase {
+    result: i32,
+    input: Vec<i32>,
+}
+
+#[cfg(test)]
+#[test]
+fn test_largest_altitude() {
+    let tests: Vec<TestCase> = vec![
+        TestCase {
+            result: 1,
+            input: vec![-5, 1, 5, 0, -7],
+        },
+        TestCase {
+            result: 0,
+            input: vec![-4, -3, -2, -1, 4, 3, 2],
+        },
+    ];
+    for t in tests {
+        assert_eq!(t.result, solution::Solution::largest_altitude(t.input));
+    }
+}
