4c9b5c5691d412b6e5743b7fab74d6f898db3dc9
dperezmavro <dio.pm@live.com>
2026-04-25 13:52:34 +0700

    added rust solution

--- easy/1732-find-the-highest-altitude/rust/src/main.rs
+++ easy/1732-find-the-highest-altitude/rust/src/main.rs
@@ -1,6 +1,12 @@
 mod solution;
 
 fn main() {
-    println!("Hello, world!");
-    let instance = solution::Solution();
+    println!(
+        "{}",
+        solution::Solution::largest_altitude(vec![-5, 1, 5, 0, -7])
+    );
+    println!(
+        "{}",
+        solution::Solution::largest_altitude(vec![-4, -3, -2, -1, 4, 3, 2])
+    );
 }
