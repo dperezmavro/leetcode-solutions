58fbcf755c440f098d5820bd197b73ffe9e69a25
dperezmavro <dio.pm@live.com>
2026-05-11 22:36:30 +0700

    WIP on rust implementations

---
+++ easy/026-remove-duplicates-from-sorted-array/rust/src/solution.rs
@@ -0,0 +1,23 @@
+pub struct Solution {}
+
+impl Solution {
+    pub fn remove_duplicates(nums: &mut Vec<i32>) -> i32 {
+        let mut k: usize = 0;
+        for (i, val) in nums.clone().iter().enumerate() {
+            if i == 0 {
+                k += 1;
+                continue;
+            }
+
+            if *val == nums[k - 1] {
+                nums[i] = -1;
+            } else {
+                nums[k] = *val;
+                k += 1;
+                nums[k] = -1;
+            }
+        }
+
+        k.try_into().unwrap()
+    }
+}
