3825717aa6ba07762865dd9f3873ac649acaf212
dperezmavro <dio.pm@live.com>
2026-05-11 22:36:30 +0700

    WIP on rust implementations

---
+++ easy/026-remove-duplicates-from-sorted-array/rust/src/main.rs
@@ -0,0 +1,11 @@
+mod solution;
+
+fn main() {
+    let mut v: Vec<i32> = vec![1, 1, 2];
+    let mut res = solution::Solution::remove_duplicates(&mut v);
+    println!("{:?} {}", v, res);
+
+    v = vec![0, 0, 1, 1, 1, 2, 2, 3, 3, 4];
+    res = solution::Solution::remove_duplicates(&mut v);
+    println!("{:?} {}", v, res);
+}
