7d0f288aed43997575ecc3e5ec0a7705478c59b4
dperezmavro <dio.pm@live.com>
2026-05-12 09:15:55 +0700

    cleanup and tests

--- easy/026-remove-duplicates-from-sorted-array/rust/src/solution.rs
+++ easy/026-remove-duplicates-from-sorted-array/rust/src/solution.rs
@@ -7,12 +7,48 @@ impl Solution {
             if nums[i] > nums[k - 1] {
                 nums[k] = nums[i];
                 k += 1;
-                // nums[k] = -1;
-            } else {
-                nums[i] = -1;
             }
         }
 
-        k.try_into().unwrap()
+        k as i32
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_basic() {
+        let mut nums = vec![1, 1, 2];
+        assert_eq!(Solution::remove_duplicates(&mut nums), 2);
+        assert_eq!(&nums[..2], &[1, 2]);
+    }
+
+    #[test]
+    fn test_longer() {
+        let mut nums = vec![0, 0, 1, 1, 1, 2, 2, 3, 3, 4];
+        assert_eq!(Solution::remove_duplicates(&mut nums), 5);
+        assert_eq!(&nums[..5], &[0, 1, 2, 3, 4]);
+    }
+
+    #[test]
+    fn test_no_duplicates() {
+        let mut nums = vec![3, 4];
+        assert_eq!(Solution::remove_duplicates(&mut nums), 2);
+        assert_eq!(&nums[..2], &[3, 4]);
+    }
+
+    #[test]
+    fn test_all_same() {
+        let mut nums = vec![1, 1, 1, 1];
+        assert_eq!(Solution::remove_duplicates(&mut nums), 1);
+        assert_eq!(&nums[..1], &[1]);
+    }
+
+    #[test]
+    fn test_single_element() {
+        let mut nums = vec![42];
+        assert_eq!(Solution::remove_duplicates(&mut nums), 1);
     }
 }
