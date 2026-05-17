af3df87ffd2fa63942fd836290dd4d71379e94fa
dperezmavro <dio.pm@live.com>
2026-05-17 13:27:49 +0700

    cleanup solution

--- hard/0004-median-of-two-sorted-arrays/go/solution.go
+++ hard/0004-median-of-two-sorted-arrays/go/solution.go
@@ -13,49 +13,36 @@ func findMedianSortedArrays(nums1 []int, nums2 []int) float64 {
 		medianIndexes[0] = len(finalArr) / 2
 	}
 
-	longestArr, shortestArr := longestArr(nums1, nums2)
-
 	i := 0
 	j := 0
-	for i < len(longestArr) {
-		for j < len(shortestArr) {
-			if i == len(longestArr) {
-				for ; j < len(shortestArr); j++ {
-					finalArr[finalArrIndex] = shortestArr[j]
-					finalArrIndex++
-				}
-				break
-			}
-
-			if longestArr[i] < shortestArr[j] {
-				finalArr[finalArrIndex] = longestArr[i]
-				i++
-			} else {
-				finalArr[finalArrIndex] = shortestArr[j]
-				j++
-			}
-			finalArrIndex++
-		}
-
-		if j == len(shortestArr) {
-			for ; i < len(longestArr); i++ {
-				finalArr[finalArrIndex] = longestArr[i]
+	for finalArrIndex < len(finalArr) {
+		if i == len(nums1) {
+			for ; j < len(nums2); j++ {
+				finalArr[finalArrIndex] = nums2[j]
 				finalArrIndex++
 			}
 			break
 		}
+		if j == len(nums2) {
+			for ; i < len(nums1); i++ {
+				finalArr[finalArrIndex] = nums1[i]
+				finalArrIndex++
+			}
+			break
+		}
+
+		if nums1[i] < nums2[j] {
+			finalArr[finalArrIndex] = nums1[i]
+			i++
+		} else {
+			finalArr[finalArrIndex] = nums2[j]
+			j++
+		}
+		finalArrIndex++
 	}
 
 	if len(finalArr)%2 == 0 {
 		return float64((finalArr[medianIndexes[0]] + finalArr[medianIndexes[1]])) / 2.0
 	}
 	return float64(finalArr[medianIndexes[0]])
-}
-
-func longestArr(nums1 []int, nums2 []int) ([]int, []int) {
-	if len(nums1) > len(nums2) {
-		return nums1, nums2
-	}
-
-	return nums2, nums1
 }
