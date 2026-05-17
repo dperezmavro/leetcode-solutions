3ca84abddc186fb87584a012fd82f47708d528f9
dperezmavro <dio.pm@live.com>
2026-05-17 13:28:34 +0700

    added stub files

---
+++ hard/0004-median-of-two-sorted-arrays/rust/src/lib.rs
@@ -0,0 +1,23 @@
+// pub fn add(left: u64, right: u64) -> u64 {
+//     left + right
+// }
+
+// #[cfg(test)]
+// mod tests {
+//     use super::*;
+
+//     #[test]
+//     fn it_works() {
+//         let result = add(2, 2);
+//         assert_eq!(result, 4);
+//     }
+// }
+
+
+pub struct Solution{}
+
+impl Solution {
+    pub fn find_median_sorted_arrays(nums1: Vec<i32>, nums2: Vec<i32>) -> f64 {
+        
+    }
+}
