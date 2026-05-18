2b245b0044fd946ac2bb288f145d20d86dc312ee
dperezmavro <dio.pm@live.com>
2026-05-18 16:47:03 +0700

    added bounded min-heap solution

--- medium/0215-kth-largest-element-in-an-array/rust/src/lib.rs
+++ medium/0215-kth-largest-element-in-an-array/rust/src/lib.rs
@@ -1,21 +1,18 @@
+use std::cmp::Reverse;
 use std::collections::BinaryHeap;
 
 pub struct Solution {}
 
 impl Solution {
     pub fn find_kth_largest(nums: Vec<i32>, k: i32) -> i32 {
-        let mut heap = BinaryHeap::new();
+        let mut min_heap = BinaryHeap::new();
         for n in nums {
-            heap.push(n);
+            min_heap.push(Reverse(n));
+            if min_heap.len() > k as usize {
+                min_heap.pop();
+            }
         }
-
-        let mut answer = 0;
-        let mut i = 0;
-        while i < k {
-            answer = heap.pop().expect("INVALID");
-            i += 1;
-        }
-        answer
+        min_heap.peek().unwrap().0
     }
 }
 
