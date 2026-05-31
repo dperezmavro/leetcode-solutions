7577d7cd6a011397bd35e41b855757abeb5a07f5
dperezmavro <dio.pm@live.com>
2026-05-31 18:35:26 +0100

    added rust solution

--- medium/0011-container-with-most-water/rust/src/lib.rs
+++ medium/0011-container-with-most-water/rust/src/lib.rs
@@ -7,7 +7,22 @@ impl Solution {
         if height.len() == 2 {
             return cmp::min(height[0], height[1]);
         }
-        0
+
+        let mut left: usize = 0;
+        let mut right: usize = height.len() - 1;
+        let mut max_volume = cmp::min(height[left], height[right]) * ((right - left) as i32);
+        while left < right {
+            if height[left] > height[right] {
+                right -= 1;
+            } else if height[left] <= height[right] {
+                left += 1;
+            }
+            let tmp_volume = cmp::min(height[left], height[right]) * ((right - left) as i32);
+            if tmp_volume > max_volume {
+                max_volume = tmp_volume;
+            }
+        }
+        max_volume
     }
 }
 
@@ -31,5 +46,11 @@ mod tests {
     fn test_3() {
         let result = Solution::max_area(vec![1, 8, 6, 2, 5, 4, 8, 3, 7]);
         assert_eq!(result, 49);
+    }
+
+    #[test]
+    fn test_4() {
+        let result = Solution::max_area(vec![8, 7, 2, 1]);
+        assert_eq!(result, 7);
     }
 }
