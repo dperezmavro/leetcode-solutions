efb3470f88c01a9c6a952f64d821a2cbe2dd8391
dperezmavro <dio.pm@live.com>
2026-06-08 17:47:38 +0200

    added tests and rewrote solution

--- hard/0004-median-of-two-sorted-arrays/python/solution.py
+++ hard/0004-median-of-two-sorted-arrays/python/solution.py
@@ -3,30 +3,36 @@ from typing import List
 
 class Solution:
     def findMedianSortedArrays(self, nums1: List[int], nums2: List[int]) -> float:
-        nums = [x for x in nums2]
-        for i in nums1:
-            nums.append(i)
-        nums.sort()
-        if len(nums) % 2 == 0:
-            midpoint = int((len(nums) - 1) / 2)
-            numA = nums[midpoint : midpoint + 2]
-            return sum(numA) / 2
+        median = 0
+
+        if len(nums1) == 0 and len(nums2) == 0:
+            return median
+
+        total_size = len(nums1) + len(nums2)
+        result = []
+        while len(nums1) != 0 or len(nums2) != 0:
+            if len(nums1) == 0:
+                result.extend(nums2)
+                break
+
+            if len(nums2) == 0:
+                result.extend(nums1)
+                break
+
+            if nums1[0] < nums2[0]:
+                el = nums1.pop(0)
+                result.append(el)
+            else:
+                el = nums2.pop(0)
+                result.append(el)
+
+            # TODO(dio): Optimisation - once we are past
+            # the midpoint of the result_array, we don't
+            # need to carry on - we can just stop there
+
+        if total_size % 2 == 0:
+            median = result[total_size // 2] + result[(total_size // 2) - 1]
+            median /= 2
         else:
-            return float(nums[int((len(nums) - 1) / 2)])
-
-    def findMedianSortedArraysSlow(self, nums1: List[int], nums2: List[int]) -> float:
-        nums = [x for x in nums2]
-        for i in nums1:
-            nums.append(i)
-        nums.sort()
-        if len(nums) % 2 == 0:
-            midpoint = int((len(nums) - 1) / 2)
-            numA = nums[midpoint : midpoint + 2]
-            return sum(numA) / 2
-        else:
-            return float(nums[int((len(nums) - 1) / 2)])
-
-
-s = Solution()
-print(s.findMedianSortedArrays([1, 3], [2]))
-print(s.findMedianSortedArrays([1, 2], [3, 4]))
+            median = result[total_size // 2]
+        return median
--- hard/0004-median-of-two-sorted-arrays/python/tests.py
+++ hard/0004-median-of-two-sorted-arrays/python/tests.py
@@ -4,7 +4,7 @@ from typing import List
 
 
 class TestCase:
-    def __init__(self, input_1: List[int], input_2: List[int], want: int):
+    def __init__(self, input_1: List[int], input_2: List[int], want: float):
         self.want = want
         self.input_1 = input_1
         self.input_2 = input_2
@@ -14,10 +14,15 @@ class TestSolution(unittest.TestCase):
     def test_equalSubstring(self):
         def assess(self, t: TestCase):
             s = Solution()
-            m = s.findMedianSortedArraysSlow(t.input_1, t.input_2)
+            m = s.findMedianSortedArrays(t.input_1, t.input_2)
             self.assertEqual(t.want, m)
 
-        tests = [TestCase([1, 3], [2], 2), TestCase([1, 2], [3, 4], 2.5)]
+        tests = [
+            TestCase([1, 3], [2], 2),
+            TestCase([1, 2], [3, 4], 2.5),
+            TestCase([1, 2], [], 1.5),
+            TestCase([1, 1], [], 1),
+        ]
 
         for t in tests:
             assess(self, t)
